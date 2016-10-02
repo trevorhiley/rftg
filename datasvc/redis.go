8697e25aca7a1372ef9e25d0d68921aaaaee6241
Trevor Hiley <trevorhiley@gmail.com>
2016-10-01 19:36:47 -0500

    redis config updates

--- datasvc/redis.go
+++ datasvc/redis.go
@@ -1,14 +1,38 @@
 package datasvc
 
-import "gopkg.in/redis.v4"
+import (
+	"net/url"
+	"os"
+
+	"gopkg.in/redis.v4"
+)
+
+func createClient() *redis.Client {
+	redisenv := os.Getenv("REDISTOGO_URL")
+	redishost := ""
+	redispass := ""
+
+	if redisenv != "" {
+		redisurl, _ := url.Parse(redisenv)
+		redishost = redisurl.Host
+		redisuser := redisurl.User
+		redispass, _ = redisuser.Password()
+	} else {
+		redishost = "localhost:6379"
+	}
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     redishost,
+		Password: redispass, // no password set
+		DB:       0,         // use default DB
+	})
+
+	return client
+}
 
 //Setkey sets a key
 func Setkey(s string) (string, error) {
-	client := redis.NewClient(&redis.Options{
-		Addr:     "sculpin.redistogo.com:10125",
-		Password: "b8581cc0a868efb154738c497aff8588", // no password set
-		DB:       0,                                  // use default DB
-	})
+	client := createClient()
 
 	err := client.Set("key", s, 0).Err()
 	if err != nil {
